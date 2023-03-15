612c7d745d215b4372d4bd54b17ac43b6a0343bb
BaccoucheTahaGit <61146148+BaccoucheTahaGit@users.noreply.github.com>
2023-03-15 21:39:57 +0100

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ATLAS_URI: "mongodb+srv://blogapp:blogapp123@cluster0.booro0s.mongodb.net/blogdb?retryWrites=true&w=majority",
+    JWT_SECRET: "aK2a56/nh4yGLWgN5RZ98b/Uaja2Vs6uXNNkKD8FvQ8=",
+    EMAIL: "idell.conn@ethereal.email",
+    PASSWORD: "TPd9QyJFZ2s4ncURKB"
+  };
---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const connect = require('./database/conn.js');
+const router = require('./router/route.js');
+const topicRoutes = require('./router/topics');
+
+const app = express();
+
+/** middlewares */
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by'); // less hackers know about our stack
+
+
+const port = 5000;
+
+/** HTTP GET Request */
+app.get('/', (req, res) => {
+    res.status(201).json("Home GET Request");
+});
+
+
+/** api routes */
+app.use('/api', router)
+app.use('/topics', topicRoutes);
+
+/** start server only when we have valid connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot connect to the server')
+    }
+}).catch(error => {
+    console.log("Invalid database connection...!");
+})
+
